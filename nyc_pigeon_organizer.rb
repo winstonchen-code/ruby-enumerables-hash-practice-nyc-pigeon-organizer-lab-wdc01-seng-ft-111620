295a9ee75d317d33e5aec1f0c37e78906b40aa9b
Winston Chen <winstonchen.wm@gmail.com>
2020-11-05 03:22:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,7 +6,7 @@ def nyc_pigeon_organizer(data)
           final_array[name] = {}
         end
         if !final_array[name][key]
-          !final_array[name][key} = []
+          !final_array[name][key] = []
         end
         final_array[name][key].push(inner_key.to_s)
       end
